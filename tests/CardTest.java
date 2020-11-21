2c5847f18c2b327bff42fcbc0820ca04802931f2
Razia Abrahams <rabrahams@groveis.com>
2020-11-21 22:58:01 +0200

    Create test class for Card class Create test class for Dealer class

---
+++ tests/CardTest.java
@@ -0,0 +1,22 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CardTest {
+
+    @Test
+    public void getCardSuit() {
+    }
+
+    @Test
+    public void setCardSuit() {
+    }
+
+    @Test
+    public void getRank() {
+    }
+
+    @Test
+    public void setRank() {
+    }
+}
---
+++ tests/DealerTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DealerTest {
+
+    @Test
+    public void checkHand() {
+    }
+
+    @Test
+    public void isStraight() {
+    }
+
+    @Test
+    public void display() {
+    }
+}
